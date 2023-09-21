7eaaeec985471a82f6c526a63f9382b1751edd7c
leo.daunbi <leothai.daunbi@gmail.com>
2023-09-21 13:00:42 +0900

    샘플데이터 추가 완료

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -5,15 +5,15 @@ include_once(__DIR__.'/class.db.php');
 
 #- db info
 define('HOSTNAME',	'localhost');
-define('USERNAME',	'test');
-define('PASSWORD',	'pass');
+define('USERNAME',	'root');
+define('PASSWORD',	'');
 define('DATABASE',	'test_employee');
 
-#- pager용 mysqli 설정
+#- pager
 $mysqli = new mysqli(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 $mysqli->set_charset("utf8");
 
-#- db 연결
+#- db 
 $dsn = 'mysql:host='.HOSTNAME.';dbname='.DATABASE.';charset=utf8';
 try {
 	$db = new db($dsn, USERNAME, PASSWORD);
@@ -22,7 +22,7 @@ try {
 	die();
 }
 
-#- Debug 함수 정의
+#- Debug
 function debug($value)
 {
 	if (empty($value)) {
@@ -78,19 +78,37 @@ try {
     $conn = new PDO("mysql:host=".HOSTNAME.";dbname=".DATABASE, USERNAME, PASSWORD);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // 100개의 샘플 데이터를 추가
+    for ($i = 0; $i < 100; $i++) {
+        $departmentName = "Department " . ($i + 1);
+
+        $sql = "INSERT INTO department (department_name) VALUES (:departmentName)";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':departmentName', $departmentName);
+        $stmt->execute();
+    }
+
+    for ($i = 0; $i < 100; $i++) {
+        $todofukenName = "Todofuken " . ($i + 1);
+
+        $sql = "INSERT INTO todofuken (todofuken_name) VALUES (:todofukenName)";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':todofukenName', $todofukenName);
+        $stmt->execute();
+    }
+
     for ($i = 0; $i < 100; $i++) {
         $name = "User " . ($i + 1);
-        $departmentId = rand(1, 10); // 부서 ID는 1부터 10까지 랜덤으로 생성
+        $departmentId = rand(1, 10); 
         $gender = ($i % 2 == 0) ? "Male" : "Female";
-        $age = rand(20, 60); // 20에서 60 사이의 나이 랜덤 생성
+        $age = rand(20, 60); 
         $email = "user" . ($i + 1) . "@example.com";
-        $zipcode = sprintf("%05d", rand(10000, 99999)); // 5자리 우편번호 랜덤 생성
-        $todofukenId = rand(1, 5); // 토도후켄 ID는 1부터 5까지 랜덤으로 생성
+        $zipcode = sprintf("%05d", rand(10000, 99999));
+        $todofukenId = rand(1, 5); 
         $address = "Address " . ($i + 1);
 
-        // SQL INSERT 문 실행
-        $sql = "INSERT INTO employees (name, department_id, gender, age, email, zip_code, todofuken_id, other_address) 
+        $sql = "INSERT INTO employee (name, department_id, gender, age, email, postal_code, todofuken_id, other_address) 
                 VALUES (:name, :departmentId, :gender, :age, :email, :zipcode, :todofukenId, :address)";
         
         $stmt = $conn->prepare($sql);
@@ -106,11 +124,12 @@ try {
         $stmt->execute();
     }
 
-    echo "100 records have been successfully added.";
+    echo "100 records have been successfully added to department, todofuken, and employee tables.";
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 
-// 데이터베이스 연결 종료
+
+
 $conn = null;
 ?>
